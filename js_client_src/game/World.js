d99d875fa35786b4632dcba3d24caa04d12ed50d
Tyler Vann-Campbell <tyler@buddy.com>
2018-05-16 10:34:11 -0700

    Remove JS files

--- js_client_src/game/World.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var World = /** @class */ (function () {
-    function World() {
-    }
-    return World;
-}());
-exports["default"] = World;
-//# sourceMappingURL=World.js.map
